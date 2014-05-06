b2e120aeec3ce6e18e072ecbf4e19a781ea66940
Rashid Khan <rashood.khan@gmail.com>
2014-05-06 18:57:53 +0400

    Added MainNotes Screen

---
+++ tomboy.ios/Screens/MainNotesScreen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace tomboy.ios
+{
+	public partial class MainNotesScreen : UIViewController
+	{
+		public MainNotesScreen () : base ("MainNotesScreen", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
